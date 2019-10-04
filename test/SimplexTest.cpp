3ec6a11f45328ab8ac6d26172122ac7d0e98c929
Daniel Tarnawski <djtarnawski@gmail.com>
2019-10-04 20:07:37 +0200

    Added checker of objective function. (#3)
    * Added checker of objective function.
    
    * Added a function that checks if the solution is optimal.

--- test/SimplexTest.cpp
+++ test/SimplexTest.cpp
@@ -6,8 +6,11 @@ struct testSimplex : public ::testing::Test
     //Given
     Simplex simplex;
     std::vector<double> emptyVector;
-    std::vector<double> vectorOf2Doubles = {1.43, -2};
+    std::vector<double> vectorOf2Doubles = {1.43, 2};
     std::vector<double> vectorOf4Doubles = {3.23, 4, -0.001, 9999999.1234};
+    std::vector<double> vectorOf5Doubles = {1.3, 7.55, -0.001, -0, 0.000000008};
+    std::vector<double> vectorOf3Doubles = {-0.5, 6.01, 1};
+    
 };
 
 TEST_F(testSimplex, testGetObjectiveFunctionWhenFunctionIsEmpty)
@@ -56,3 +59,53 @@ TEST_F(testSimplex, testAdd2ConstraintFunctionWhenGivenFunctionsHaveParams)
     ASSERT_EQ(simplex.getConstraintFunction(0), vectorOf2Doubles);
     ASSERT_EQ(simplex.getConstraintFunction(1), vectorOf4Doubles);
 }
+
+TEST_F(testSimplex, testCheckObjectiveFunctionIsSolvableByDualSimplexWhenFunctionIsEmpty)
+{
+    //Then
+    ASSERT_FALSE(simplex.checkObjectiveFunctionIsSolvableByDualSimplex());
+}
+
+TEST_F(testSimplex, testCheckObjectiveFunctionIsSolvableByDualSimplexWhenFunctionParametersAreCorrect)
+{
+    //When
+    simplex.setObjectiveFunction(vectorOf2Doubles);
+    //Then
+    ASSERT_TRUE(simplex.checkObjectiveFunctionIsSolvableByDualSimplex());
+}
+
+TEST_F(testSimplex, testCheckObjectiveFunctionIsSolvableByDualSimplexWhenFunctionParametersAreIncorrect)
+{
+    //When
+    simplex.setObjectiveFunction(vectorOf4Doubles);
+    //Then
+    ASSERT_FALSE(simplex.checkObjectiveFunctionIsSolvableByDualSimplex());
+}
+
+TEST_F(testSimplex, testCheckSolutionIsOptimalWhenParametersAreCorrect)
+{
+    //When
+    simplex.addConstraintFunction(vectorOf2Doubles);
+    simplex.addConstraintFunction(vectorOf4Doubles);
+    simplex.addConstraintFunction(vectorOf5Doubles);
+    //Then
+    ASSERT_TRUE(simplex.checkSolutionIsOptimal());
+}
+
+TEST_F(testSimplex, testCheckSolutionIsOptimalWhenParametersAreIncorrect)
+{
+    //When
+    simplex.addConstraintFunction(vectorOf2Doubles);
+    simplex.addConstraintFunction(vectorOf4Doubles);
+    simplex.addConstraintFunction(vectorOf3Doubles);
+    simplex.addConstraintFunction(vectorOf5Doubles);
+    //Then
+    ASSERT_FALSE(simplex.checkSolutionIsOptimal());
+}
+
+TEST_F(testSimplex, testCheckSolutionIsOptimalWhenIsWithoutConstraints)
+{
+    //Then
+    ASSERT_TRUE(simplex.checkSolutionIsOptimal());
+}
+
